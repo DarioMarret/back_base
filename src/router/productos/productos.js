c60eeb42bdfb48284995d7bb67a95daf5b8178a1
DJMarret <javier_dario_marret@hotmail.com>
2023-08-18 21:35:05 -0500

    2023-08-18_21:35:05 listar y actualizar usuarios caja

--- src/router/productos/productos.js
+++ src/router/productos/productos.js
@@ -6,7 +6,7 @@ const routes = Router()
 routes.post('/Cargar_producto_excel', CargarProductosDesdeExcel)
 routes.post('/cargar_producto_unitario',CrearProductounitario)
 routes.get('/listar_produtos_empresa',ListarProducto)
-routes.get('/listar_produtos_categorias',ListarProductosPorCategoria)
+routes.post('/listar_produtos_categorias',ListarProductosPorCategoria)
 
 routes.post('/busqueda_coinsidencia',ListarProductoConsiDencia)
 routes.delete('/eliminar_producto_id',EliminarProductoPorId)
