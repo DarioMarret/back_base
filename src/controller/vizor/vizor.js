0142f478a8972956b04556ebebaa90226b55c25b
DJMarret <javier_dario_marret@hotmail.com>
2023-08-19 18:53:20 -0500

    2023-08-19_18:53:20 listar y actualizar usuarios caja

--- src/controller/vizor/vizor.js
+++ src/controller/vizor/vizor.js
@@ -1,13 +1,15 @@
+import moment from "moment";
 import { sql } from "../../database/conexion";
 
 
 export const RegistrarOrden = async (req, res) => {
     const { orden, usuario } = req.body
+    let fecha = moment().format('YYYY-MM-DD HH:mm:ss')
     try {
         for (let index = 0; index < orden.length; index++) {
             const items = orden[index];
-            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, usuario) VALUES 
-            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', '${usuario}')`
+            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, usuario, fecha) VALUES 
+            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', '${usuario}', '${fecha}')`
             await sql.query(query)            
         }
         res.json({ message: 'Orden registrada' })
