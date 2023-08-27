1ad1a99c01a2ddde9f7173f8ce00a50e8e6ec2fe
DJMarret <javier_dario_marret@hotmail.com>
2023-08-26 21:05:47 -0500

    2023-08-26_21:05:47 listar y actualizar usuarios caja

--- src/controller/vizor/vizor.js
+++ src/controller/vizor/vizor.js
@@ -6,10 +6,19 @@ export const RegistrarOrden = async (req, res) => {
     const { orden, usuario } = req.body
     let fecha = moment().format('YYYY-MM-DD HH:mm:ss')
     try {
+        // listar cual es la ultima orden de la empresa
+        let query = `SELECT * FROM orden WHERE empresa = '${orden[0].empresa}' ORDER BY id DESC LIMIT 1`
+        const response = await sql.query(query)
+        var numero_ordenes = 1
+        if (response[0].length > 0) {
+            numero_ordenes = response[0][0].orden + 1
+        }
+
+
         for (let index = 0; index < orden.length; index++) {
             const items = orden[index];
-            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, usuario, fecha) VALUES 
-            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', '${usuario}', '${fecha}')`
+            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, orden, usuario, fecha) VALUES 
+            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', ${numero_ordenes}, '${usuario}', '${fecha}')`
             await sql.query(query)            
         }
         res.json({ success: true, message: 'Orden registrada con exito' })
