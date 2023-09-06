b799796320275d446644646bd2b10415c7e6a563
DJMarret <javier_dario_marret@hotmail.com>
2023-09-06 09:29:35 -0500

    2023-09-06_09:29:35 listar y actualizar usuarios caja

--- src/controller/vizor/vizor.js
+++ src/controller/vizor/vizor.js
@@ -3,7 +3,7 @@ import { sql } from "../../database/conexion";
 
 
 export const RegistrarOrden = async (req, res) => {
-    const { orden, usuario } = req.body
+    const { orden, usuario, mesa } = req.body
     let fecha = moment().format('YYYY-MM-DD HH:mm:ss')
     try {
         // listar cual es la ultima orden de la empresa
@@ -16,12 +16,12 @@ export const RegistrarOrden = async (req, res) => {
 
         for (let index = 0; index < orden.length; index++) {
             const items = orden[index];
-            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, orden, usuario, fecha) VALUES 
-            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', ${numero_ordenes}, '${usuario}', '${fecha}')`
-            await sql.query(query)            
+            let query = `INSERT INTO orden (auxiliar, producto, precio_venta, porcentaje_iva, porcentaje, empresa, cantidad, opt, random, orden, usuario, fecha, mesa) VALUES 
+            ('${items.auxiliar}', '${items.producto}', ${parseFloat(items.precio_venta)}, ${parseFloat(items.porcentaje_iva)}, ${items.porcentaje}, '${items.empresa}', ${items.cantidad}, '${items.opt}', '${items.random}', ${numero_ordenes}, '${usuario}', '${fecha}', '${mesa}')`
+            await sql.query(query)
         }
 
-        res.json({ success: true, message: 'Orden registrada con exito', orden: numero_ordenes })
+        res.json({ success: true, message: 'Orden registrada con exito', orden: numero_ordenes, mesa: mesa })
     } catch (error) {
         console.log(error)
         res.json({
@@ -71,6 +71,87 @@ export const EliminarOrden = async (req, res) => {
     const { empresa, random } = req.params
     try {
         let query = `DELETE FROM orden WHERE empresa = '${empresa}' AND random = '${random}'`
+        const response = await sql.query(query)
+        res.json({
+            success: true,
+            data: response
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            data: error
+        })
+    }
+}
+
+
+// mesas
+export const ListarMesas = async (req, res) => {
+    const { empresa } = req.body
+    try {
+        let query = `SELECT * FROM mesas WHERE empresa = '${empresa}'`
+        const response = await sql.query(query)
+        res.json({
+            success: true,
+            data: response[0]
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            data: error
+        })
+    }
+}
+
+export const RegistrarMesa = async (req, res) => {
+    const { empresa, mesa } = req.body
+    try {
+        let query = `INSERT INTO mesas (empresa, mesa) VALUES ('${empresa}', '${mesa}')`
+        const response = await sql.query(query)
+        res.json({
+            success: true,
+            data: response
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            data: error
+        })
+    }
+}
+
+export const EliminarMesa = async (req, res) => {
+    const { id, empresa } = req.params
+    try {
+        // verificar que la mesa este disponible para eliminarla
+        let me = `SELECT * FROM orden WHERE empresa = '${empresa}' AND id = '${id}'`
+        const response = await sql.query(me)
+        if (response[0].length > 0 && response[0][0].estado == "Disponible") {
+            return res.json({
+                success: false,
+                data: 'La mesa no se puede eliminar porque tiene ordenes activas'
+            })
+        }
+
+        let query = `DELETE FROM mesas WHERE empresa = '${empresa}' AND id = '${id}'`
+        await sql.query(query)
+        res.json({
+            success: true,
+            data: "Mesa eliminada con exito"
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            data: error
+        })
+    }
+}
+
+export const CambiarEstadoMesa = async (req, res) => {
+    const { id, estado, mesero } = req.body
+    try {
+        let fecha = moment().format('YYYY-MM-DD HH:mm:ss')
+        let query = `UPDATE mesas SET estado = '${estado}', mesero = '${mesero}', fecha = '${fecha}' WHERE id = '${id}'`
         const response = await sql.query(query)
         res.json({
             success: true,
--- src/router/vizor/vizor.js
+++ src/router/vizor/vizor.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { EliminarOrden, EliminarProductoOrden, ListarOrdenes, RegistrarOrden } from '../../controller/vizor/vizor'
+import { CambiarEstadoMesa, EliminarMesa, EliminarOrden, EliminarProductoOrden, ListarMesas, ListarOrdenes, RegistrarMesa, RegistrarOrden } from '../../controller/vizor/vizor'
 
 const routes = Router()
 
@@ -7,5 +7,11 @@ routes.post('/vizor', RegistrarOrden)
 routes.post('/listar_vizor',ListarOrdenes)
 routes.delete('/eliminar_items/:id',EliminarProductoOrden)
 routes.delete('/eliminar_item/:empresa/:random',EliminarOrden)
+// mesas
+routes.post('/mesas', ListarMesas)
+routes.post('/registra_mesas', RegistrarMesa)
+routes.delete('/elimina_mesa/:id/:empresa', EliminarMesa)
+routes.post('/atender_mesa', CambiarEstadoMesa)
+
 
 export default routes
