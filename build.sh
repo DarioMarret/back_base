8a35da87b3f5683875fdbc804bf8f95578b9e9f6
DJMarret <javier_dario_marret@hotmail.com>
2023-12-05 20:49:34 -0500

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 git pull
-npm run build
-pm2 restart 3
+git add .
+git commit -m "update"
+git push
+
+export DOCKER_IMAGE_VERSION="dev_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
+
+
+docker login -u "djmarret1992" -p "Tumadre1@" docker.io
+
+docker build -t djmarret1992/codigobm-api-ordenfacil:${DOCKER_IMAGE_VERSION} -f Dockerfile .
+docker tag djmarret1992/codigobm-api-ordenfacil:${DOCKER_IMAGE_VERSION} djmarret1992/codigobm-api-ordenfacil:latest
+docker push djmarret1992/codigobm-api-ordenfacil:${DOCKER_IMAGE_VERSION}
+docker push djmarret1992/codigobm-api-ordenfacil:latest
+echo "tag: ${DOCKER_IMAGE_VERSION}"
